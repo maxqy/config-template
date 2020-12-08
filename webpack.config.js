04ff684b0ba0635981e23e948c634e0b2c6f5d4d
Max Davydovsky <max.davydovsky@gmail.com>
2020-12-08 19:09:44 +0300

    Update Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
@@ -20,15 +19,12 @@ module.exports = {
       favicon: "./src/images/favicon.ico",
       template: `${__dirname}/src/index.html`,
     }),
-    new MiniCssExtractPlugin({
-      filename: "bundle.css",
-    }),
   ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
