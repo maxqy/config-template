cc2043a7ae2c9e1e3a41332ee74eda462474d03f
Max Davydovsky <max.davydovsky@gmail.com>
2021-02-16 23:08:49 +0300

    feat: add babel

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,18 @@ module.exports = {
       template: `${__dirname}/src/index.html`,
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
