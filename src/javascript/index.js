e16a506e950e07b3be21c4c49e3019b446e5053c
Max Davydovsky <max.davydovsky@gmail.com>
2021-03-11 00:37:12 +0300

    feat: enable HMR

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,3 +2,7 @@ import '../sass/main.scss';
 import hello from './vendor';
 
 hello();
+
+if (module.hot) {
+  module.hot.accept();
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,8 +4,10 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  target: 'web',
   devServer: {
     contentBase: './dist',
+    hot: true,
   },
   module: {
     rules: [
